260a8528be1342d8ea667c9c8842b453d78b542d
Phong Nguyen <phongnguyen180993@gmail.com>
2017-03-02 19:36:54 +0700

    set up firebase with auth

--- app/src/main/java/vn/com/phongnguyen93/week3fbdemo/MainActivity.java
+++ app/src/main/java/vn/com/phongnguyen93/week3fbdemo/MainActivity.java
@@ -1,16 +1,26 @@
 package vn.com.phongnguyen93.week3fbdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-
+  public static final String TAG = MainActivity.class.getSimpleName();
+  private FirebaseAuth mAuth;
+  private FirebaseAuth.AuthStateListener mAuthListener;
+  private String userId;
 
   EditText mEdtMessage;
   Button mBtnSend;
@@ -19,19 +29,77 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    mEdtMessage = (EditText)findViewById(R.id.edt_message);
-    mBtnSend = (Button)findViewById(R.id.btn_send);
+    setupFirebase();
+
+    mEdtMessage = (EditText) findViewById(R.id.edt_message);
+    mBtnSend = (Button) findViewById(R.id.btn_send);
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    final DatabaseReference myRef = database.getReference("message");
 
     mBtnSend.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-//        myRef.setValue();
-
+      @Override public void onClick(View v) {
+        if(userId!=null){
+          myRef.setValue(userId+" - "+mEdtMessage.getText().toString());
+        }
 
       }
     });
+  }
+
+  @Override protected void onResume() {
+    super.onResume();
+    signinFirebase();
+  }
+
+  private void signinFirebase() {
+    mAuth.signInAnonymously()
+        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+            Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+            // If sign in fails, display a message to the user. If sign in succeeds
+            // the auth state listener will be notified and logic to handle the
+            // signed in user can be handled in the listener.
+            if (!task.isSuccessful()) {
+              Log.w(TAG, "signInAnonymously", task.getException());
+              Toast.makeText(MainActivity.this, "Authentication failed.",
+                  Toast.LENGTH_SHORT).show();
+            }
+
+            // ...
+          }
+        });
+  }
+
+  private void setupFirebase() {
+    mAuth = FirebaseAuth.getInstance();
+    mAuthListener = new FirebaseAuth.AuthStateListener() {
+      @Override public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+          // User is signed in
+          userId = user.getUid();
+          Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+        } else {
+          // User is signed out
+          Log.d(TAG, "onAuthStateChanged:signed_out");
+        }
+        // ...
+      }
+    };
+  }
+
+  @Override protected void onStart() {
+    super.onStart();
+    mAuth.addAuthStateListener(mAuthListener);
+  }
+
+  @Override protected void onStop() {
+    super.onStop();
+    if (mAuthListener != null) {
+      mAuth.removeAuthStateListener(mAuthListener);
+    }
   }
 }

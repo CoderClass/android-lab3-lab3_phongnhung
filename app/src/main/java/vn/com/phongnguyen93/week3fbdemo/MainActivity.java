8eb336884df7e00b7dfa16f98c918630ba9d2ff8
Hong Nhung <hongnhung@Hongs-MacBook-Pro.local>
2017-03-02 19:20:27 +0700

    edit text

--- app/src/main/java/vn/com/phongnguyen93/week3fbdemo/MainActivity.java
+++ app/src/main/java/vn/com/phongnguyen93/week3fbdemo/MainActivity.java
@@ -2,17 +2,36 @@ package vn.com.phongnguyen93.week3fbdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+
+  EditText mEdtMessage;
+  Button mBtnSend;
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mEdtMessage = (EditText)findViewById(R.id.edt_message);
+    mBtnSend = (Button)findViewById(R.id.btn_send);
+
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference myRef = database.getReference("message");
 
-    myRef.setValue("Hello, World!");
+    mBtnSend.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+//        myRef.setValue();
+
+
+      }
+    });
   }
 }
